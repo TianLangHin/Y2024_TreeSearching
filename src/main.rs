2979a42b1854d34c26615b3877bb091721628b2f
TianLangHin <tianlanghin@gmail.com>
2024-03-29 22:16:05 +1100

    Added testing of all tree sizes in Musczyka & Shinghal (1985)

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,43 @@ pub mod games;
 pub mod prelude;
 pub mod search;
 
+use colored::Colorize;
 use rayon::prelude::*;
 use seq_macro::seq;
-use colored::Colorize;
 
 use std::time::Instant;
+
+// Depth-Width pair for setting each hypothetical tree configuration.
+type TreeSetting = (usize, usize);
+
+// The typing for the list of settings to be tested,
+// but arranged as a tuple since this is required by const-ness in seq_macro.
+type AllTreeSettings = (
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+    TreeSetting,
+);
 
 fn test_stockman_tree() {
     let handler = StockmanHandler::new(());
@@ -58,7 +90,10 @@ fn perft_div_main(depth: usize, pos: ChessPos, handler: &ChessHandler) {
         .get_legal_moves(pos)
         .map(|mv| {
             let num = perft(depth - 1, pos.play_move(mv), handler);
-            println!("{}: {num}", handler.move_string(mv, (pos.squares >> 19) & 1));
+            println!(
+                "{}: {num}",
+                handler.move_string(mv, (pos.squares >> 19) & 1)
+            );
             num
         })
         .sum();
@@ -81,7 +116,10 @@ fn perft_div_main_par(depth: usize, pos: ChessPos, handler: &ChessHandler) {
         .par_iter()
         .map(|&mv| {
             let num = perft(depth - 1, pos.play_move(mv), handler);
-            println!("{}: {num}", handler.move_string(mv, (pos.squares >> 19) & 1));
+            println!(
+                "{}: {num}",
+                handler.move_string(mv, (pos.squares >> 19) & 1)
+            );
             num
         })
         .sum();
@@ -92,21 +130,25 @@ fn perft_div_main_par(depth: usize, pos: ChessPos, handler: &ChessHandler) {
 fn eval_from_line<THandler, TPosition, const SIZE: usize>(
     handler: &THandler,
     initial_pos: TPosition,
-    line: [Option<<TPosition as GamePosition>::Move>; SIZE]
+    line: [Option<<TPosition as GamePosition>::Move>; SIZE],
 ) -> <THandler as GameHandler<TPosition>>::Eval
 where
     THandler: GameHandler<TPosition>,
     TPosition: GamePosition,
 {
     let mut pos = initial_pos;
-    let mut depth = SIZE;
+    let mut depth = 0;
     for &mv in line.iter() {
         if let Some(m) = mv {
             pos = pos.play_move(m);
-            depth -= 1;
+            depth += 1;
         }
     }
-    handler.evaluate(pos, depth, SIZE)
+    if (depth & 1) == 0 {
+        handler.evaluate(pos, SIZE - depth, SIZE)
+    } else {
+        -handler.evaluate(pos, SIZE - depth, SIZE)
+    }
 }
 
 fn root_call_bb<THandler, TPosition, const DEPTH: usize>(
@@ -152,12 +194,7 @@ where
     THandler: GameHandler<TPosition>,
     TPosition: GamePosition,
 {
-    p_alpha_beta(
-        handler,
-        root,
-        DEPTH,
-        DEPTH,
-    )
+    p_alpha_beta(handler, root, DEPTH, DEPTH)
 }
 
 fn root_call_pvs<THandler, TPosition, const DEPTH: usize>(
@@ -186,12 +223,7 @@ where
     THandler: GameHandler<TPosition>,
     TPosition: GamePosition,
 {
-    scout(
-        handler,
-        root,
-        DEPTH,
-        DEPTH,
-    )
+    scout(handler, root, DEPTH, DEPTH)
 }
 
 fn root_call_sss<THandler, TPosition, const DEPTH: usize>(
@@ -202,12 +234,26 @@ where
     THandler: GameHandler<TPosition>,
     TPosition: GamePosition,
 {
-    sss(
-        handler,
-        root,
-        DEPTH,
-        DEPTH,
-    )
+    sss(handler, root, DEPTH, DEPTH)
+}
+
+fn raw_moves_display<T, const SIZE: usize>(
+    move_list: [Option<T>; SIZE]
+) -> String
+where
+    T: std::fmt::Debug
+{
+    move_list
+        .iter()
+        .map_while(|mv| {
+            if let Some(m) = mv {
+                Some(format!("{:?}", m))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(", ")
 }
 
 fn test_algorithms<THandler, TPosition, const DEPTH: usize>(
@@ -240,17 +286,31 @@ where
         "sss",
     );
 
+    let mut results: Vec<MoveAndPV<THandler, TPosition, DEPTH>> = Vec::new();
+
     println!("{}", position_name.bright_magenta());
     seq!(N in 0..6 {
         println!("{}", algorithm_names.N.bright_cyan());
         let s = Instant::now();
         let result: MoveAndPV<THandler, TPosition, DEPTH> = algorithms.N(&handler, startpos);
         println!("Time elapsed: {} ms", s.elapsed().as_millis().to_string().bright_yellow());
-        if eval_from_line(&handler, startpos, result.1) == result.0 {
-            println!("Line and eval {}", "MATCH".bright_green());
+        let recalculated_eval = eval_from_line(&handler, startpos, result.1);
+        if recalculated_eval == result.0 {
+            println!("Eval and Line {}", "MATCH".bright_green());
+            println!(
+                "Eval: {}, Line: {}",
+                format!("{:?}", result.0).bright_green(),
+                raw_moves_display(result.1).bright_green(),
+            );
         } else {
-            println!("Line and eval {}", "MISMATCH".bright_red());
+            println!("Eval and Line {}", "MISMATCH".bright_red());
+            println!(
+                "Expected Eval: {}, Got Eval: {}",
+                format!("{:?}", result.0).bright_green(),
+                format!("{:?}", recalculated_eval).bright_red(),
+            );
         }
+        results.push(result);
     });
 }
 
@@ -265,15 +325,47 @@ fn main() {
         },
         (),
     );
-    test_algorithms::<UnordIndHypTreeHandler, HypTreePos, 8>(
-        "Unordered-Independent Hypothetical Game Tree (Depth = 8, Width = 8)",
-        HypTreeParams {
-            depth: 8,
-            width: 8,
-            seed: 314159,
-        },
-        8,
+
+    const DEPTH_WIDTH_PAIRS: AllTreeSettings = (
+        (2, 2),
+        (2, 3),
+        (2, 4),
+        (2, 5),
+        (2, 6),
+        (2, 8),
+        (2, 10),
+        (2, 24),
+        (3, 2),
+        (3, 3),
+        (3, 4),
+        (3, 5),
+        (3, 6),
+        (3, 8),
+        (3, 10),
+        (4, 2),
+        (4, 3),
+        (4, 4),
+        (4, 5),
+        (5, 2),
+        (5, 3),
+        (5, 4),
+        (6, 2),
+        (6, 3),
     );
 
-
+    seq!(N in 0..24 {
+        test_algorithms::<UnordIndHypTreeHandler, HypTreePos, { DEPTH_WIDTH_PAIRS.N.0 }>(
+            &format!(
+                "Unordered-Independent Hypothetical Game Tree (Depth = {}, Width = {})",
+                DEPTH_WIDTH_PAIRS.N.0,
+                DEPTH_WIDTH_PAIRS.N.1,
+            ),
+            HypTreeParams {
+                depth: DEPTH_WIDTH_PAIRS.N.0,
+                width: DEPTH_WIDTH_PAIRS.N.1,
+                seed: 314159,
+            },
+            8,
+        );
+    });
 }
